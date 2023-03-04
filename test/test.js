cc3404107ce2ae1d4aee3c317bf10efde20d93b5
Marck Carrión Guzmán <mcarrion@kcramsolutions.com>
2023-03-04 21:35:27 +0100

    feat: agregado log name y funciones de string
    - [x] wrapText -> para funciones de texto: alieando y formato
    - [x] formatNumber -> formateo de números

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
+const { wrapLeft, wrapRight, wrapCenter, formatNumber } = require('../src/helpers');
 const { Logger, LEVEL } = require('../src/main')
 
-const logger = new Logger();
+const logger = new Logger('Perron');
+const log1 = new Logger('asdad asdadsa sad');
 
 console.log('%s| LOG LEVEL: DEBUG |%s',"-".repeat(15), "-".repeat(15));
 Logger.level = LEVEL.DEBUG
@@ -56,3 +58,17 @@ logger.log('Hola from log')
 logger.warn('Hola from warn')
 logger.error('Hola from error')
 logger.critical('Hola from critical')
+
+
+Logger.level = LEVEL.LOG;
+logger.log('Text Wrapper: size: 20:', wrapLeft('Hola', 12, 2, '='), 'End of message')
+logger.log('Text Wrapper: size: 20:', wrapRight('Hola', 12, 2, '='), 'End of message')
+logger.log('Text Wrapper: size: 20:', wrapCenter('Hola', 12, 2, '='), 'End of message')
+logger.log('Text Wrapper: size: 20:', wrapCenter('Holas', 12, 2, '='), 'End of message')
+logger.log('Text Wrapper: size: 20:', wrapLeft('Hola', 11, 2, '='), 'End of message')
+logger.log('Text Wrapper: size: 20:', wrapRight('Hola', 11, 2, '='), 'End of message')
+logger.log('Text Wrapper: size: 20:', wrapCenter('Hola', 8, 2, '='), 'End of message')
+logger.log('Text Wrapper: size: 20:', wrapCenter('Holasadsasd', 11, 2, '='), 'End of message')
+
+logger.log('\n\t', formatNumber(1, 0), ' => 001\n\t',formatNumber(9,1), ' => 09' );
+
